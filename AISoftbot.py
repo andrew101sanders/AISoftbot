026cadc81ae4b5544bee4ce91c446bae3bcb51b8
andrew101sanders <andrew101sanders@gmail.com>
2019-11-25 08:28:43 -0500

    refactor

--- AISoftbot.py
+++ AISoftbot.py
@@ -4,75 +4,37 @@ import numpy as np
 
 file = open('C:/users/andrew/desktop/input.txt')
 filelines = file.readlines()
-inputmatrix = []
-S = []
-G = []
-markedseeds = []
+inputmatrix = []  # [y][x] - [Rows][Columns] required for matplot
+S = []  # Original seeds from first two rows in format [x,y]
+G = []  # Surrounding seeds
+Marked = []  # Seeds that have already been explored
 
 
-def checkdup(dup):
-    if dup not in G and dup not in markedseeds and dup not in S:
-        G.append(dup)
+def checksurrounding(node):  # [X, Y]
+    print('hi')
 
 
-for line in filelines:
+# populate inputmatrix
+for x in filelines:
     temp = []
-    for char in line:
-        if char != '\n':
-            temp.append(int(char))
+    for y in x:
+        if y != '\n':
+            temp.append(int(y))
     inputmatrix.append(temp)
 
-i = 0
-for seed in inputmatrix[0]:
-    if seed == 0:
-        S.append([i, 0])
-    i += 1
+# populate S with first row of inputmatrix
+for index, firstrowitem in enumerate(inputmatrix[0]):
+    if inputmatrix[0][index] == 0:
+        S.append([0, index])
 
-i = 0
-for seed in inputmatrix[1]:
-    if seed == 0:
-        S.append([i, 1])
-    i += 1
+# populate S with second row of inputmatrix
+for index, firstrowitem in enumerate(inputmatrix[1]):
+    if inputmatrix[1][index] == 0:
+        S.append([1, index])
 
-for startseed in S:
-    marked = False
-    if startseed in markedseeds:
-        marked = True
-    if not marked:
-        markedseeds.append(startseed)
-        left = startseed[0] - 1 >= 0
-        top = startseed[1] - 1 >= 0
-        right = startseed[0] + 1 <= 19
-        bottom = startseed[1] - 1 <= 19
-        if left:  # left side
-            if inputmatrix[startseed[0] - 1][startseed[1]] == 0:  # middle left
-                checkdup([startseed[0] - 1, startseed[1]])
-            if top:  # top left
-                if inputmatrix[startseed[0] - 1][startseed[1] - 1] == 0:
-                    checkdup([startseed[0] - 1, startseed[1] - 1])
-            if bottom:  # bottom left
-                if inputmatrix[startseed[0] - 1][startseed[1] + 1] == 0:
-                    checkdup([startseed[0] - 1, startseed[1] + 1])
-        if top:  # top middle
-            if inputmatrix[startseed[0]][startseed[1] - 1] == 0:
-                checkdup([startseed[0], startseed[1] - 1])
-        if bottom:  # bottom middle
-            if inputmatrix[startseed[0]][startseed[1] + 1] == 0:
-                checkdup([startseed[0], startseed[1] + 1])
-        if right:  # right side
-            if inputmatrix[startseed[0] + 1][startseed[1]] == 0:  # middle right
-                checkdup([startseed[0] + 1, startseed[1]])
-            if top:  # top right
-                if inputmatrix[startseed[0] + 1][startseed[1] - 1] == 0:
-                    checkdup([startseed[0] + 1, startseed[1] - 1])
-            if bottom:  # bottom right
-                if inputmatrix[startseed[0] + 1][startseed[1] + 1] == 0:
-                    checkdup([startseed[0] + 1, startseed[1] + 1])
+for initialseeds in S:
+    print(initialseeds)
 
-print('inputmatrix: ' + str(inputmatrix))
-print('S (original seeds from first 2 rows): ' + str(S))
-print('G (surroundings of original seeds): ' + str(G))
-print('markedseeds: ' + str(markedseeds))
 
 ax = plt.subplot()
 cmap = colors.ListedColormap(['red', 'blue'])
